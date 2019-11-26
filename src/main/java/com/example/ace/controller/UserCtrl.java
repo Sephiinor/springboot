3ce15612516c2c4aa35eccd097f2d21ddef7868f
Wx <w229084881.vip.qq.com>
2019-11-26 14:29:06 +0800

    Session存入redis

--- src/main/java/com/example/ace/controller/UserCtrl.java
+++ src/main/java/com/example/ace/controller/UserCtrl.java
@@ -5,6 +5,10 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
 /**
  * <p>Title:UserCtrl.java</p >
  * <p>Description:controller</p >
@@ -23,4 +27,31 @@ public class UserCtrl {
         System.err.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
         return user;
     }
+
+    //将session存入redis中实现session共享
+    @RequestMapping("/uid")
+    String uid(HttpSession session){
+        UUID uid = (UUID) session.getAttribute("uid");
+        if(uid == null){
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid",uid);
+        return session.getId();
+    }
+
+    @RequestMapping("/login")
+    public String login (HttpServletRequest request,String userName ,String password){
+        String msg = "----login failure----";
+        User user = new User();
+        user.setUserName("wx");
+        user.setPassword("123");
+
+        if( password.equals(user.getPassword())){
+            request.getSession().setAttribute("user",user);
+            msg = "----login success----";
+        }
+        return msg;
+
+    }
+
 }
